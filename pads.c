afb93ca96db327ce8176f20c84b95ca4e1e13870
James Booth <boothj5@gmail.com>
2012-10-16 00:34:24 +0100

    Refactored into functions

--- pads.c
+++ pads.c
@@ -1,24 +1,64 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <ncurses.h>
 
 #define PAD_SIZE 200
 
+static int rows;
+static int cols;
+static int prow;
+
+static WINDOW *pad;
+
+static void init_ncurses(void);
+static void init_pad(void);
+static void read_file(void);
+static void handle_input(void);
+static void handle_exit(void);
+
 int main()
 {   
+    atexit(handle_exit);
+    init_ncurses();
+    init_pad();
+    
+    read_file();
+
+    // prefresh(*pad, pminrow, pmincol, sminrow, smincol, smaxrow, smaxcol);
+    prefresh(pad, prow, 0, 2, 4, rows - 3 , cols - 5);
+    wtimeout(pad, -1);
+
+    while(TRUE) {
+        handle_input();
+    }
+
+    endwin();
+
+    return 0;
+}
+
+static void 
+init_ncurses(void)
+{
     initscr();
     cbreak();
-
     start_color();
     init_pair(1, COLOR_WHITE, COLOR_BLUE);
-    
-    int rows, cols;
-    int prow = 0;
     getmaxyx(stdscr, rows, cols);
+}
 
-    WINDOW *pad = newpad(PAD_SIZE, cols);
+static void
+init_pad(void)
+{
+    pad = newpad(PAD_SIZE, cols);
     wbkgd(pad, COLOR_PAIR(1));
     keypad(pad, TRUE);
+    prow = 0;
+}
 
+static void
+read_file(void)
+{
     FILE *gpl = fopen("COPYING", "r");
     char *line = NULL;
     size_t read = 0;
@@ -35,34 +75,37 @@ int main()
         }
         fclose(gpl);
    }
-
-    // prefresh(*pad, pminrow, pmincol, sminrow, smincol, smaxrow, smaxcol);
-    prefresh(pad, prow, 0, 2, 4, rows - 3 , cols - 5);
-    wtimeout(pad, -1);
-
-    while(TRUE) {
-        int ch = wgetch(pad);
-
-        switch(ch) {
-            case KEY_UP:
-                prow--;
-                if (prow < 0) {
-                    prow = 0;
-                }
-                prefresh(pad, prow, 0, 2, 4, rows - 3 , cols - 5);
-                break;
-            case KEY_DOWN:
-                prow++;
-                if (prow > PAD_SIZE) {
-                    prow = PAD_SIZE;
-                }
-                prefresh(pad, prow, 0, 2, 4, rows - 3 , cols - 5);
-                break;
-            default:
-                break;
-        }
-    }
-    endwin();
-
-    return 0;
 }
+
+static void
+handle_input(void)
+{
+    int ch = wgetch(pad);
+
+    switch(ch) {
+        case KEY_UP:
+            prow--;
+            if (prow < 0) {
+                prow = 0;
+            }
+            prefresh(pad, prow, 0, 2, 4, rows - 3 , cols - 5);
+            break;
+        case KEY_DOWN:
+            prow++;
+            if (prow > PAD_SIZE) {
+                prow = PAD_SIZE;
+            }
+            prefresh(pad, prow, 0, 2, 4, rows - 3 , cols - 5);
+            break;
+        default:
+            break;
+    }
+}
+
+static void
+handle_exit(void)
+{
+    endwin();
+    printf("\nQuit successfully.\n\n");
+}
+
