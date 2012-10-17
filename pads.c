60fc9b981ccf0e19bf40d528d2a09065d7dcdff7
James Booth <boothj5@gmail.com>
2012-10-17 23:18:28 +0100

    Stop at bottom of pad

--- pads.c
+++ pads.c
@@ -3,7 +3,7 @@
 #include <ncurses.h>
 
 // Pad rows are numbered 0 to PAD_SIZE-1
-#define PAD_SIZE 200
+#define PAD_ROWS 200
 
 // number of rows and cols on the screen
 // numbered 0 to rows-1 and 0 to cols-1
@@ -11,10 +11,10 @@ static int rows;
 static int cols;
 
 // current pad row
-static int prow;
+static int current_pad_row;
 
-// number of visible rows of pad
-static int visible_pad_height;
+// visible pad rows
+static int visible_pad_rows;
 
 static WINDOW *pad;
 
@@ -54,15 +54,16 @@ init_ncurses(void)
     start_color();
     init_pair(1, COLOR_WHITE, COLOR_BLUE);
     getmaxyx(stdscr, rows, cols);
+    visible_pad_rows = rows - 4;
 }
 
 static void
 init_pad(void)
 {
-    pad = newpad(PAD_SIZE, cols);
+    pad = newpad(PAD_ROWS, cols);
     wbkgd(pad, COLOR_PAIR(1));
     keypad(pad, TRUE);
-    prow = 0;
+    current_pad_row = 0;
 }
 
 static void
@@ -75,7 +76,7 @@ read_file(void)
     
     if (gpl != NULL) {
         while ((getline(&line, &read, gpl) != -1) && 
-                (line_num < PAD_SIZE)) {
+                (line_num < PAD_ROWS)) {
             wprintw(pad, "%d: %s", line_num, line);
             free(line);
             line = NULL;
@@ -93,18 +94,18 @@ handle_input(void)
 
     switch(ch) {
         case KEY_UP:
-            prow--;
-            if (prow < 0) {
-                prow = 0;
+            current_pad_row--;
+            if (current_pad_row < 0) {
+                current_pad_row = 0;
             }
-            pad_draw(prow);
+            pad_draw(current_pad_row);
             break;
         case KEY_DOWN:
-            prow++;
-            if (prow > PAD_SIZE) {
-                prow = PAD_SIZE;
+            current_pad_row++;
+            if (current_pad_row + visible_pad_rows > PAD_ROWS) {
+                current_pad_row--;
             }
-            pad_draw(prow);
+            pad_draw(current_pad_row);
             break;
         default:
             break;
@@ -125,6 +126,6 @@ pad_draw(int pad_row)
         // starting row and column of pad
         pad_row, 0,
         // screen coords of pad
-        2, 4, rows - 3 , cols - 5);
+        2, 0, rows - 3 , cols - 1);
 }
 
